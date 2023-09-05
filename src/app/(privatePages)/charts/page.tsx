6046ff3cfc5680a6e938059e37e9a2b959e3d703
marinahambaryan <marinahambaryan99@gmail.com>
2023-09-05 11:14:37 +0400

    Added Charts

--- src/app/(privatePages)/charts/page.tsx
+++ src/app/(privatePages)/charts/page.tsx
@@ -1,9 +1,30 @@
 import React from "react";
+import { Box } from "@mui/material";
+
+import LineChart from "./components/LineChart";
+import BarChart from "./components/BarChart";
+import DoughnutChart from "./components/DoughnutChart";
 
 type Props = {};
 
+const styles = {
+  marginTop: "30px",
+};
+
 const page = (props: Props) => {
-  return <div>page</div>;
+  return (
+    <Box>
+      <Box style={styles}>
+        <LineChart />
+      </Box>
+      <Box style={styles}>
+        <BarChart />
+      </Box>
+      <Box style={styles}>
+        <DoughnutChart />
+      </Box>
+    </Box>
+  );
 };
 
 export default page;
